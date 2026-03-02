a777c7b0ebf1b5b511c521547ce5636e3aab7d3f
ennaciriyahya1 <ennaciri.yahya1@gmail.com>
2026-03-02 14:54:22 +1100

    Update BAM_to_FASTQ.sh
    Comments added

--- Scripts/BAM_to_FASTQ.sh
+++ Scripts/BAM_to_FASTQ.sh
@@ -3,17 +3,20 @@
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 
+#The environment where my packages are downloaded
 conda activate unsw
 
+#The directory containing the BAM files downloaded from CCLE 
 cd /scratch/zq45/yn0830/AML_RNA_BAM
 
 mkdir -p fastq_output
 
+#Selects each bam file from all the bam files contained in the folder
 for bam in *.bam
 do
     sample=$(basename "$bam" .bam)
 
-    samtools fastq -@ 8 \
+    samtools fastq -@ 8 \ #Creates Read 1 and Read 2 Files from the BAM file
         -1 fastq_output/${sample}_R1.fastq.gz \
         -2 fastq_output/${sample}_R2.fastq.gz \
         -0 /dev/null -s /dev/null -n \
