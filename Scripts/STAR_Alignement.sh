28bbf250a3e4b07c2b429384134dde2b0f21009d
ennaciriyahya1 <ennaciri.yahya1@gmail.com>
2026-03-02 15:00:58 +1100

    Update STAR_Alignement.sh
    Comments added

--- Scripts/STAR_Alignement.sh
+++ Scripts/STAR_Alignement.sh
@@ -8,10 +8,11 @@ conda activate unsw
 scratchDir="/srv/scratch/z5752982/"
 outputDir="/srv/scratch/z5752982/Test_alignement"
 
+#Performs alignment on the Reference Genome
 STAR \
 	--runThreadN 16 \
 	--genomeDir "${scratchDir}STAR-aligner/Index-Creation" \
 	--readFilesIn "${scratchDir}Test_FASTQ/ENCFFR1.fastq.gz" "${scratchDir}Test_FASTQ/ENCFFR2.fastq.gz" \
 	--readFilesCommand zcat \
-	--twopassMode Basic \
+	--twopassMode Basic \ #Important for AS detection, as it runs two steps of SJ identification
  	--outFileNamePrefix "${outputDir}/"
