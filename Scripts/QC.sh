3bc0d4beca8c3dc8ec64f6713c94cfb4ae1a5af2
ennaciriyahya1 <ennaciri.yahya1@gmail.com>
2026-03-04 11:49:07 +1100

    Update QC.sh
    Comments

--- Scripts/QC.sh
+++ Scripts/QC.sh
@@ -8,5 +8,6 @@ conda activate unsw
 cd /scratch/zq45/yn0830/AML_RNA_BAM/fastq_output
 
 mkdir -p FASTQC
-
+#Runs a Quality control on fastq files
+#Important after BAM to Fastq step because it allows to discard poor quality samples
 fastqc -t 16 -o FASTQC *.fastq.gz
