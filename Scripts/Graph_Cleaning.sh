646eb49deb795f12ca1da60a6c857816e37f3b44
ennaciriyahya1 <ennaciri.yahya1@gmail.com>
2026-03-04 11:45:11 +1100

    Update Graph_Cleaning.sh
    Comments

--- Scripts/Graph_Cleaning.sh
+++ Scripts/Graph_Cleaning.sh
@@ -9,5 +9,7 @@ dirpath="/scratch/zq45/yn0830/AML_RNA_BAM/fastq_output"
 
 cd ${dirpath}
 
+#The input file (the graph) must be added at the end of the command line
+#This step allows for suppression of the k-mers with lower counts, probably arising from errors in sequencing
 metagraph clean -v -p 16 --to-fasta --prune-tips 62 --prune-unitigs 0 --fallback 2 \
                 -o clean_test_graph testgraph.dbg
