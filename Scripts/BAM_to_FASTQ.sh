2de760c1c018773c756ac0a542f515683e1bc925
ennaciriyahya1 <ennaciri.yahya1@gmail.com>
2026-03-04 11:47:42 +1100

    Update BAM_to_FASTQ.sh
    Comments

--- Scripts/BAM_to_FASTQ.sh
+++ Scripts/BAM_to_FASTQ.sh
@@ -5,10 +5,12 @@ source $CONDA_BASE/etc/profile.d/conda.sh
 
 conda activate unsw
 
+#The directory containing the BAM files downloaded from CCLE 
 cd /scratch/zq45/yn0830/AML_RNA_BAM
 
 mkdir -p fastq_output
 
+#Selects each bam file from all the bam files contained in the folder
 for bam in *.bam
 do
     sample=$(basename "$bam" .bam)
@@ -19,3 +21,4 @@ do
         -0 /dev/null -s /dev/null -n \
         "$bam"
 done
+#Creates Read 1 and Read 2 Files from the BAM file
