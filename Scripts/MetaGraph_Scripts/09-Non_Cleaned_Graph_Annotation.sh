b17936f2e26f2488be8a5582d8f4aa32d6b5611b
ennaciriyahya1 <ennaciri.yahya1@gmail.com>
2026-03-12 13:02:12 +1100

    New Organization

--- Scripts/MetaGraph_Scripts/09-Non_Cleaned_Graph_Annotation.sh
+++ Scripts/MetaGraph_Scripts/09-Non_Cleaned_Graph_Annotation.sh
@@ -11,7 +11,7 @@ cd ${dirpath}
 
 #-Anno_filename allows to annotate the k-mers depending on their file of origin (useful for sequence detection) and implements the counts using --count-kmers
 
-metagraph annotate -v -i ${scratch}MetaGraph_Results/Original_Graphs/T_ALL_Graphs/T_ALL_Graph.dbg --anno-filename --count-kmers --count-width 16 \
---disk-swap ${scratch}MetaGraph_Swap --mem-cap-gb 500 -p 48 -o ${scratch}MetaGraph_Results/Annotated_Graphs/T-ALL/Non_Cleaned_Graph_annotation \
+metagraph annotate -v -i ${scratch}MetaGraph_Results/Original_Graphs/AML_Graphs/T_ALL_Graph.dbg --anno-filename --count-kmers --count-width 16 \
+--disk-swap ${scratch}MetaGraph_Swap --mem-cap-gb 500 -p 48 -o ${scratch}MetaGraph_Results/Annotated_Graphs/AML/Non_Cleaned_Graphs_Annotation/Non_Cleaned_Graph_annotation \
 *_R1.fastq.gz *_R2.fastq.gz
 
